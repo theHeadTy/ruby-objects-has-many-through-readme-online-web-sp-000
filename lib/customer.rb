0b9b67818b4f98aa83be9877323c8f6e1f001632
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 16:37:20 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,10 +11,6 @@ class Customer
     @@all
   end
 
-  def new_meal(waiter, total, tip)
-    Meal.new(waiter, self, total, tip)
-  end
-
   def meals
     Meal.all.select { |meal| meal.customer == self }
   end
