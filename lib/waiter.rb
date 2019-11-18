ed95768173340d014af4499a7c889b6ac472660b
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 16:34:22 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,21 @@
 class Waiter
+  attr_accessor :name, :years
+  @@all = []
+  def initialize(name, years)
+    @name = name
+    @years = years
+    self.class.all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.waiter == self }
+  end
+
+  def best_tipper
+    Meal.all.sort_by { |meal| meal.tip }.last
+  end
 end
