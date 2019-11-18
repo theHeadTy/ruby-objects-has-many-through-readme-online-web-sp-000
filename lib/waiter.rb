313e54b63bd7ef3107213c63f8bd4beca9c6b342
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 16:40:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -11,9 +11,8 @@ class Waiter
     @@all
   end
   
-  def new_meal(waiter, total, tip)
-    Meal.new(waiter, self, total, tip)
-  end
+  def new_meal(customer, total, tip)
+    Meal.new()
 
   def meals
     Meal.all.select { |meal| meal.waiter == self }
@@ -23,3 +22,13 @@ class Waiter
     Meal.all.sort_by { |meal| meal.tip }
   end
 end
+
+      it "initializes a meal using the current Waiter instance, a provided Customer instance and a total and tip" do
+        howard = Customer.new("Howard", 30)
+        terrance = Waiter.new("Terrance", 1)
+        terrance.new_meal(howard, 10, 1)
+        
+        expect(Meal.all.first.waiter).to eq(terrance)
+        expect(Meal.all.first.customer).to eq(howard)
+      end
+    end
