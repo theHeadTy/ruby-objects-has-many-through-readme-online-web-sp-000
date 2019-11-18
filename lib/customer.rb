d3acd85410fbbc5b1aa4c9dcb6e48c3072026ded
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 16:51:10 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
 class Customer
+  attr_accessor :name, :age
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
   
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.customer == self }
+  end
+
+  # it "returns an Array of Waiter instances associated with this customer's meals" do
+  def waiters
+    w_arr = []
+    Meal.all.each do |meal| 
+      if meal.customer == self 
+        w_arr << meal.waiter
+      end
+    end
+    w_arr
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
 class Waiter
+  attr_accessor :name, :years
+  @@all = []
+  def initialize(name, years)
+    @name = name
+    @years = years
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.waiter == self }
+  end
+  
+  def best_tipper
+    tip = Meal.all.max_by { |meal| meal.tip }
+    
+    tip.customer
+    
+  end
 
 end
